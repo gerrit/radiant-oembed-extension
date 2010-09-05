5e9b2627f2a403842a1ba1f890ac36e53ee15c92
Gerrit Kaiser <gerrit@gerritkaiser.de>
2010-09-05 22:03:49 +0100

    introduced hackish <r:oembed:url> tag that allows to set URL from another radius tag

--- app/models/o_embed_tags.rb
+++ app/models/o_embed_tags.rb
@@ -15,21 +15,44 @@ module OEmbedTags
     <pre><code><r:oembed url="http://www.flickr.com/photos/notsogoodphotography/503637906/" /></code></pre>
   }
   tag 'oembed' do |tag|
-    raise TagError if tag.attr['url'].blank?
-    provider = OEmbed::Providers::Embedly
-    tag.locals.oembed = provider.get(tag.attr.delete('url'), tag.attr)
+    tag.locals.oembed_url, tag.locals.oembed_opts = tag.attr.delete('url'), tag.attr
+    tag.locals.debug = 'here'
     if tag.double?
       tag.expand
     else
-      tag.locals.oembed.html
+      oembed(tag).html
     end
+  end
+  
+=begin
+  # Not documented as its a somewhat advanced usage
+  # also not sure about the design of this yet.
+  
+  desc %{
+    Set the URL of the embedded object.
+    
+    Can be used as an alternative to the +url+ attribute on +r:oembed+
+  
+    *Usage:*
+    
+    <pre><code>
+      <r:oembed>
+        <r:url><r:content part="flickr_url" /></r:url>
+        <r:html />
+      </r:oembed>
+    </pre></code>
+  }
+=end
+  tag 'oembed:url' do |tag|
+    tag.locals.object.oembed_url = tag.expand.strip
+    ''
   end
   
   desc %{
     Renders a thumbnail image for the parent r:oembed tag
   }
   tag 'oembed:thumbnail' do |tag|
-    url, width = tag.locals.oembed.thumbnail_url, tag.locals.oembed.thumbnail_width
+    url, width = oembed(tag).thumbnail_url, oembed(tag).thumbnail_width
     %{<img src="#{url}" width="#{width}">}
   end
   
@@ -37,10 +60,10 @@ module OEmbedTags
     Renders the default representation for the parent embedded object sent by the provider
   }
   tag 'oembed:html' do |tag|
-    tag.locals.oembed.html
+    oembed(tag).html
   end
   
-  %w[title description url html
+  %w[title description html
     thumbnail_url thumbnail_width
     author_name author_url
     provider_name provider_url].each do |attr|
@@ -48,7 +71,14 @@ module OEmbedTags
       Renders the #{attr.humanize} sent by the Provider
     }
     tag "oembed:#{attr}" do |tag|
-      tag.locals.oembed.send(attr)
+      oembed(tag).send(attr)
     end
+  end
+
+private
+  def oembed(tag)
+    url, opts = tag.locals.oembed_url, tag.locals.oembed_opts
+    raise TagError, 'please specify a url attribute on the r:oembed tag' unless url
+    tag.locals.oembed ||= OEmbed::Providers::Embedly.get(url, opts)
   end
 end
